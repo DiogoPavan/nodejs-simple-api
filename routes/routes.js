6e1085b6b48affeeca6827ffb226682c1d6c0359
Renan Azzolim <azzolimrenan2@gmail.com>
2020-01-26 17:35:31 -0300

    added get certificate info function

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,22 @@
+const pem = require("pem");
+const fs = require("fs");
+
+
 var appRouter = function (app) {
-    app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+
+  app.get("/getCertificateInfo", function (req, res) {
+    const pfx = fs.readFileSync('/media/renan/1TB/test.pfx');
+
+    pem.readPkcs12(pfx, { p12Password: 'patrick01'}, (err, cert) => {
+      pem.readCertificateInfo(cert.cert, (err, resp) => {
+        res.status(200).send(resp);
+      }, (err) => {
+        res.status(400).send(err);
+      });
     });
-  }
+  });
+
   
-  module.exports = appRouter;
+}
+
+module.exports = appRouter;
